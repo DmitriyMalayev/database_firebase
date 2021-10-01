d01fe09568aa9523f75ea96f4acbe13733552f7c
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-09-30 21:40:33 -0400

    Real Time Listeners with Add Delete functionality

--- app.js
+++ app.js
@@ -1,28 +1,70 @@
 const list = document.querySelector("ul");
-const addRecipe = (recipe) => {
+const form = document.querySelector("form");
+const button = document.querySelector("button");
+
+const addRecipe = (recipe, id) => {
   let html = `
-  <li>
+  <li data-id="${id}">
   <div>${recipe.title}</div>
-  <div>${recipe.created_at.toDate()}</div> 
+  <div>${recipe.created_at.toDate()}</div>
+  <button class="btn btn-danger btn-sm my-2">delete</button>
   </li>`;
   list.innerHTML += html;
 };
 
-db.collection("recipes")
-  .get()
-  .then((snapshot) => {
-    snapshot.docs.forEach((doc) => {
-      addRecipe(doc.data());
-    });
-  })
-  .catch((err) => {
-    console.log(err);
+const deleteRecipe = (id) => {
+  const recipes = document.querySelectorAll("li");
+  recipes.forEach((recipe) => {
+    if (recipe.getAttribute("data-id") === id) {
+      recipe.remove();
+    }
   });
+};
 
-/* 
-reference to collection. 
-get() is an asynchronous task which returns a promise
-snapshot is how a collection looks like at that moment in time. 
-  docs is an array with information
+//Real Time Listener
+const unsub = db.collection("recipes").onSnapshot((snapshot) => {
+  snapshot.docChanges().forEach((change) => {
+    const doc = change.doc;
+    if (change.type === "added") {
+      addRecipe(doc.data(), doc.id);
+    } else if (change.type === "removed") {
+      deleteRecipe(doc.id);
+    }
+  });
+});
+//Add documents
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const now = new Date();
+  const recipe = {
+    title: form.recipe.value,
+    created_at: firebase.firestore.Timestamp.fromDate(now),
+  };
+  db.collection("recipes")
+    .add(recipe)
+    .then(() => {
+      console.log("recipe added");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+// Delete Document
+list.addEventListener("click", (e) => {
+  if (e.target.tagName === "button") {
+    const id = e.target.parentElement.getAttribute("data-id");
+    db.collection("recipes")
+      .doc(id)
+      .delete()
+      .then(() => {
+        console.log("recipe deleted");
+      })
+      .catch(err);
+  }
+});
 
-*/
+//Unsubscribe from Database Changes by invoking unsub()
+button.addEventListener("click", () => {
+  unsub();
+  console.log("unsubscribed");
+});
--- notes.md
+++ notes.md
@@ -22,3 +22,40 @@ Database Types
     Firebase is known as BackEnd As a Service
     Documents look like JavaScript Objects
     Firestore 
+
+
+
+reference to collection. 
+get() is an asynchronous task which returns a promise
+snapshot is how a collection looks like at that moment in time. 
+  docs is an array with information
+onSnapshot 
+  fires a callback function and takes in the snapshot of when it occurs. 
+  Any time there is a change in the collection, fireStore takes a snapshot of the collection and fires the callback function. 
+  In the snapshot, we can see all of the data changes.  docChanges method shows all the changes that occured in the database.
+Properties
+  We don't have to specify all of the properties.
+created_at: firebase.firestore.Timestamp.fromDate(now),   
+  creating a Timestamp Object based on the new Date()
+add
+  Used to add a new document object to the collection
+  It's asynchronous 
+db.collection("recipes")   => Getting a reference to the collection
+  .doc(id)    => reference to a single document, pass in the id we want a reference to
+  .delete()   => async method to delete a document from the collection
+  .then(() => { 
+    console.log("recipe deleted");
+  })
+  .catch(err);
+
+Get Documents    Removed because we want to use real time listeners. This is a single request. 
+db.collection("recipes")
+  .get()
+  .then((snapshot) => {
+    snapshot.docs.forEach((doc) => {
+      addRecipe(doc.data(), doc.id); //add recipe
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
